3ce0c4590e5658d23288a4d5001c0875590633ca
Gustacardoso <gustimba17@gmail.com>
2020-03-26 13:21:37 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Car");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "gu", "Gus@", new Date(), 2000.0, obj);
+		System.out.println(seller);
 
 	}
 
