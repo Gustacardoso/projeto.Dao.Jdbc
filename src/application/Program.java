3c00eff5b05d28351daf26dfefa83d6a8635e9f4
gustavo cardoso <gusta_tamy@hotmail.com>
2020-03-30 08:36:21 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.Dao.DaoFactory;
+import model.Dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,11 +11,19 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Car");
+		/*Department obj = new Department(1, "Car");
 		
 		Seller seller = new Seller(21, "gu", "Gus@", new Date(), 2000.0, obj);
-		System.out.println(seller);
-
+		System.out.println(seller);*/
+		
+		/*desta forma o meu  pragrama nao  conhece a implemetação,
+           conhece so mente a interface, é tambem uma forma de mostrar as dependecia sem espliciatar a implementação   */
+	SellerDao sellerDao = DaoFactory.createSellerDao();
+	
+	Seller seller = sellerDao.findById(3);
+	
+	System.out.println(seller);
+	  
 	}
 
 }
--- src/model/Dao/DaoFactory.java
+++ src/model/Dao/DaoFactory.java
@@ -1,12 +1,13 @@
 package model.Dao;
 
+import db.DB;
 import model.Dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
    
 	//classe tera operção static para instanciar os daos
-	
+	// internamento ela implemetar um interfaces retornando uma classe de implementação
 	 public static SellerDao createSellerDao() {
-		 return new SellerDaoJDBC();
+		 return new SellerDaoJDBC(DB.getConnection());
 	 }
 }
